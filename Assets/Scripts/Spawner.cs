ac3da8c2f3f3822530824ca984fb57e346f10f25
Robertas Paulauskas <paulauskx@gmail.com>
2021-06-17 11:36:26 +0300

    init project

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement;
+
+public class Spawner : MonoBehaviour
+{
+    public AssetReference _asset;
+
+    public void Spawn()
+    {
+        
+        var asset = _asset.InstantiateAsync();
+        Debug.Log(asset.Result);
+    }
+
+    public void Despawn()
+    {
+        _asset.ReleaseAsset();
+    }
+}
