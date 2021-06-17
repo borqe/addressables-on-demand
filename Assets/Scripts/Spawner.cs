6990e566a2873a7abd4cdb0e8bb56a7cb0b3b6b1
Robertas Paulauskas <paulauskx@gmail.com>
2021-06-17 17:18:05 +0300

    progress so far, build still not working :(

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,21 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement;
+using UnityEngine.Networking;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Spawner : MonoBehaviour
 {
-    public AssetReference _asset;
+    public AssetReference asset;
+
+    public List<GameObject> loadedObjects = new List<GameObject>();
+
+    private void Start() {
+        NetworkTransport.Init();
+
+        ConnectionConfig config = new ConnectionConfig();
+        HostTopology topology = new HostTopology(config, 10);
+
+        int hostId = NetworkTransport.AddHost(topology, 50655);
+
+        byte error;
+
+        var connectionId = NetworkTransport.Connect(hostId, "192.168.1.64", 50655, 0, out error);
+
+        Debug.Log(connectionId);
+    }
 
     public void Spawn()
     {
-        
-        var asset = _asset.InstantiateAsync();
-        Debug.Log(asset.Result);
+        Debug.Log(asset);
+        Addressables.LoadAssetAsync<GameObject>(asset).Completed += OnLoaded;
+    }
+
+    private void OnLoaded(AsyncOperationHandle<GameObject> obj)
+    {
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            var instantiatedObject = Instantiate(obj.Result);
+            loadedObjects.Add(instantiatedObject);
+        }
     }
 
     public void Despawn()
     {
-        _asset.ReleaseAsset();
+        asset.ReleaseAsset();
     }
 }
