a120684e08bfd4bdef35bcd98df00903b47e1a13
Robertas Paulauskas <paulauskx@gmail.com>
2021-06-18 16:19:31 +0300

    clear + download starter

---
+++ Assets/Scripts/Downloader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class Downloader : MonoBehaviour
+{
+    public void Download() => StartCoroutine(Setup());
+
+    private IEnumerator Setup()
+    {
+        string key = "assetKey";
+        //Clear all cached AssetBundles
+        Addressables.ClearDependencyCacheAsync(key);
+
+        Debug.Log("clear cache");
+
+        //Check the download size
+        AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(key);
+        yield return getDownloadSize;
+
+        Debug.Log("get download size");
+
+        //If the download size is greater than 0, download all the dependencies.
+        if (getDownloadSize.Result > 0)
+        {
+            AsyncOperationHandle downloadDependencies = Addressables.DownloadDependenciesAsync(key);
+            yield return downloadDependencies;
+
+            downloadDependencies.Completed += OnDownloadComplete;
+        }
+    }
+
+    private void OnDownloadComplete(AsyncOperationHandle obj)
+    {
+
+        // TODO: Enable spawn button
+        Debug.Log("download done");
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,52 +1,27 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.Networking;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Spawner : MonoBehaviour
 {
     public AssetReference asset;
-
     public List<GameObject> loadedObjects = new List<GameObject>();
-
-    // private void Start() => StartCoroutine(Setup());
-
-    private IEnumerator Setup()
-    {
-        string key = "assetKey";
-        //Clear all cached AssetBundles
-        Addressables.ClearDependencyCacheAsync(key);
-
-        Debug.Log("clear cache");
-
-        //Check the download size
-        AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(key);
-        yield return getDownloadSize;
-
-        Debug.Log("get download size");
-
-        //If the download size is greater than 0, download all the dependencies.
-        if (getDownloadSize.Result > 0)
-        {
-            AsyncOperationHandle downloadDependencies = Addressables.DownloadDependenciesAsync(key);
-            yield return downloadDependencies;
-
-            downloadDependencies.Completed += OnDownloadComplete;
-        }
-    }
-
-    private void OnDownloadComplete(AsyncOperationHandle obj)
-    {
-        Debug.Log("download done");
-    }
 
     public void Spawn()
     {
-        Debug.Log(asset);
         Addressables.LoadAssetAsync<GameObject>(asset).Completed += OnLoaded;
+    }
+
+    public void Clear()
+    {
+        foreach (var obj in loadedObjects)
+        {
+            Destroy(obj.gameObject);
+        }
+
+        loadedObjects.Clear();
+        asset.ReleaseAsset();
     }
 
     private void OnLoaded(AsyncOperationHandle<GameObject> obj)
@@ -56,10 +31,5 @@ public class Spawner : MonoBehaviour
             var instantiatedObject = Instantiate(obj.Result);
             loadedObjects.Add(instantiatedObject);
         }
-    }
-
-    public void Despawn()
-    {
-        asset.ReleaseAsset();
     }
 }
