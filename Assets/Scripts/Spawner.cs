fde24be71cf55750794b37b157f935b89686075e
Robertas Paulauskas <paulauskx@gmail.com>
2021-06-18 16:03:47 +0300

    get asset bundles from ftp

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -10,6 +11,37 @@ public class Spawner : MonoBehaviour
     public AssetReference asset;
 
     public List<GameObject> loadedObjects = new List<GameObject>();
+
+    // private void Start() => StartCoroutine(Setup());
+
+    private IEnumerator Setup()
+    {
+        string key = "assetKey";
+        //Clear all cached AssetBundles
+        Addressables.ClearDependencyCacheAsync(key);
+
+        Debug.Log("clear cache");
+
+        //Check the download size
+        AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(key);
+        yield return getDownloadSize;
+
+        Debug.Log("get download size");
+
+        //If the download size is greater than 0, download all the dependencies.
+        if (getDownloadSize.Result > 0)
+        {
+            AsyncOperationHandle downloadDependencies = Addressables.DownloadDependenciesAsync(key);
+            yield return downloadDependencies;
+
+            downloadDependencies.Completed += OnDownloadComplete;
+        }
+    }
+
+    private void OnDownloadComplete(AsyncOperationHandle obj)
+    {
+        Debug.Log("download done");
+    }
 
     public void Spawn()
     {
