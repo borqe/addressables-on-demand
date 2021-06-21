512bd7e2610c9b8a2a0930f945028b0582a40ce2
Robertas Paulauskas <paulauskx@gmail.com>
2021-06-21 17:59:20 +0300

    progress?

--- Assets/Scripts/Downloader.cs
+++ Assets/Scripts/Downloader.cs
@@ -1,40 +1,70 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.UI;
 
 public class Downloader : MonoBehaviour
 {
+    public Button spawnButton;
+    public List<AssetLabelReference> labelReferences = new List<AssetLabelReference>();
+
+    public object assetBundleKey;
+
     public void Download() => StartCoroutine(Setup());
 
     private IEnumerator Setup()
     {
-        string key = "assetKey";
+        Addressables.InitializeAsync().Completed += OnInitialize;
+
+        yield return assetBundleKey;
+
+        // //If the download size is greater than 0, download all the dependencies.
+        // if (getDownloadSize.Result > 0)
+        // {
+        //     AsyncOperationHandle downloadDependencies = Addressables.DownloadDependenciesAsync(assetKey, true);
+        //     downloadDependencies.Completed += OnDownloadComplete;
+            
+        //     yield return downloadDependencies;
+        // }
+
+        yield return null;
+    }
+
+    private void OnInitialize(AsyncOperationHandle<IResourceLocator> obj)
+    {
+        Debug.Log("initialization complete");
+
+        assetBundleKey = obj.Result.Keys.First();
+
         //Clear all cached AssetBundles
-        Addressables.ClearDependencyCacheAsync(key);
+        Caching.ClearCache();
+        Addressables.ClearDependencyCacheAsync(assetBundleKey);
 
-        Debug.Log("clear cache");
+        StartCoroutine(GetDownloadSize());
 
-        //Check the download size
-        AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(key);
+        // foreach (var itemKey in obj.Result.Keys)
+        // {
+        //     Debug.Log(itemKey);
+        // }
+    }
+
+    private IEnumerator GetDownloadSize()
+    {
+        // Check the download size
+        AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(assetBundleKey);
         yield return getDownloadSize;
 
-        Debug.Log("get download size");
-
-        //If the download size is greater than 0, download all the dependencies.
-        if (getDownloadSize.Result > 0)
-        {
-            AsyncOperationHandle downloadDependencies = Addressables.DownloadDependenciesAsync(key);
-            yield return downloadDependencies;
-
-            downloadDependencies.Completed += OnDownloadComplete;
-        }
+        Debug.Log(getDownloadSize.Result);
     }
 
     private void OnDownloadComplete(AsyncOperationHandle obj)
     {
-
-        // TODO: Enable spawn button
         Debug.Log("download done");
+        spawnButton.interactable = true;
     }
 }
